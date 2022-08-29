28858d9c0017c3c2a68d58d3feaf1456d254f9ea
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-08-29 15:33:52 -0400

    adds table and useState to Home

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [status, setStatus] = useState('Read');
+
+    const handleStatus = () => {
+        if (status === 'Read') {
+            setStatus('Unread');
+        } else {
+            setStatus('Read');
+        }
+    }
     return ( 
-        <h1>Homepage</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Pages</th>
+                    <th>Read?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>300</td>
+                    <td><button onClick={handleStatus}>{ status }</button></td>
+                    <td><button>Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
      );
 }
  
