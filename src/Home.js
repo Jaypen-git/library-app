f33e1b092afcaada91ccc843b373ac04a91bbe3a
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-08-29 15:44:42 -0400

    home componenet maps a list of books

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 const Home = () => {
     const [status, setStatus] = useState('Read');
+    const [books, setBooks] = useState([
+        { id: 1, title: 'Book 1', author: 'Author 1', pages: 300, status: 'Read'},
+        { id: 2, title: 'Book 2', author: 'Author 2', pages: 280, status: 'Read'},
+        { id: 3, title: 'Book 3', author: 'Author 3', pages: 330, status: 'Unread'}
+    ])
 
     const handleStatus = () => {
         if (status === 'Read') {
@@ -21,13 +26,15 @@ const Home = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Test</td>
-                    <td>Test</td>
-                    <td>300</td>
-                    <td><button onClick={handleStatus}>{ status }</button></td>
-                    <td><button>Delete</button></td>
-                </tr>
+                {books.map(book => (
+                    <tr key={ book.id }>
+                        <td>{ book.title }</td>
+                        <td>{ book.author }</td>
+                        <td>{ book.pages }</td>
+                        <td><button onClick={handleStatus}>{ book.status }</button></td>
+                        <td><button>Delete</button></td>
+                    </tr>
+                ))}
             </tbody>
         </table>
      );
