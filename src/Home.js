b068c26564496fdfb3ab8e34770f18be86a709de
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-08-29 21:27:25 -0400

    adds error catching

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,33 @@ import BookList from "./BookList";
 
 const Home = () => {
     const [books, setBooks] = useState(null);
-    const [isPending, setIsPending] = useState(true)
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => { // useEffect runs a function on every render
         setTimeout(() => {
             fetch('http://localhost:8000/library')
             .then(res => {
                 if (!res.ok) {
-                    throw new Error("Couldn't fetch the data");
+                    throw Error("Couldn't fetch the data");
                 }
                 return res.json();
             })
             .then(data => {
                 setBooks(data);
                 setIsPending(false);
+                setError(null);
             })
             .catch(err => {
-                console.log(err);
+                setIsPending(false);
+                setError(err.message);
             })
         }, 1000);
     }, []); // useEffect dependencies are triggers for useEffect to run. If left empty, it will only run initially
     
     return ( 
         <div className="content">
+            { error && <div>Could not fetch the data</div>}
             {isPending && <div>Loading...</div>}
             {/* reusable component for outputting a book list, passes down booklist and handle delete function as props */}
             {books && <BookList books={ books }/>}
