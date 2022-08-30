07f05d37ead8043e60186f9c4540404b303935e9
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-08-29 22:03:20 -0400

    adds abort controller to prevent abort errors

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     const {data: books, isPending, error} = useFetch('http://localhost:8000/library');
     return ( 
         <div className="content">
-            { error && <div>Could not fetch the data</div>}
+            {error && <div>Could not fetch the data</div>}
             {isPending && <div>Loading...</div>}
             {/* reusable component for outputting a book list, passes down booklist and handle delete function as props */}
             {books && <BookList books={ books }/>}
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => { // useEffect runs a function on every render
+        const abortCont = new AbortController(); //this aborts an attempt to fetch data
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
                     throw Error("Couldn't fetch the data");
@@ -20,10 +22,15 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                }
                 setIsPending(false);
                 setError(err.message);
-            })
+            });
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]); // useEffect dependencies are triggers for useEffect to run. If left empty, it will only run initially
 
     return {data, isPending, error};
