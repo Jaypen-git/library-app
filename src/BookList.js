660df72915216b9e4dbd060c647ff78a84a3c991
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-09-08 15:09:43 -0400

    styles booklist for screens no smaller than 640px

--- src/BookList.js
+++ src/BookList.js
@@ -39,22 +39,22 @@ const BookList = ({books}) => {
                         }
                     }}>
                         {/* {console.log(book.title.length)} */}
-                        <p className="col-span-4 text-3xl font-medium px-1.5 ml-2 main-title">
+                        <p className="col-span-4 text-3xl sm:text-4xl font-medium px-1.5 ml-2 main-title">
                             { book.title.length > 14 ? shortenTitle(book.title) : book.title }
                         </p>
                         <Link to={"/edit/" + book.id}>
-                            <button className="col-span-1 bg-blue-600 m-2 w-12 h-12 text-center rounded flex justify-center align-center">
-                                <img src={require("./images/pencil.png")} alt="edit" className="w-10 h-10 my-1"/>
+                            <button className="col-span-1 bg-blue-600 m-2 w-12 h-12 sm:w-14 sm:h-14 text-center rounded flex justify-center align-center">
+                                <img src={require("./images/pencil.png")} alt="edit" className="w-10 h-10 sm:w-12 sm:h-12 my-1"/>
                             </button>
                         </Link>
-                        <button className="col-span-1 bg-red-600 m-2 w-12 h-12 text-center rounded flex justify-center align-center" onClick={e => {handleDelete(e.target.parentNode.parentNode.id)}}>
-                            <img src={require("./images/delete.png")} alt="delete" className="w-10 h-10 my-1"/>
+                        <button className="col-span-1 bg-red-600 m-2 w-12 h-12 sm:w-14 sm:h-14 text-center rounded flex justify-center align-center" onClick={e => {handleDelete(e.target.parentNode.parentNode.id)}}>
+                            <img src={require("./images/delete.png")} alt="delete" className="w-10 h-10 sm:w-12 sm:h-12 my-1"/>
                         </button>
                         <div className="details hidden col-span-6">
-                            <p className="col-span-4 text-2xl font-medium px-1.5 ml-2">{ `Title: ${book.title}` }</p>
-                            <p className="col-span-4 text-2xl font-medium px-1.5 ml-2">{ `Author: ${book.author}` }</p>
-                            <p className="col-span-4 text-2xl font-medium px-1.5 ml-2">{ `Pages: ${book.pages}` }</p>
-                            <p className="col-span-4 text-2xl font-medium px-1.5 ml-2">{ `Status: ${book.status}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Title: ${book.title}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Author: ${book.author}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Pages: ${book.pages}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Status: ${book.status}` }</p>
                         </div>
                     </div>
             ))}
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const Header = ({link1}) => {
+const Header = () => {
     return ( 
         <header className="bg-red-400 p-4 sm:p-6 flex items-center justify-between">
             <div className="flex items-center">
@@ -8,7 +8,7 @@ const Header = ({link1}) => {
                 <h1 className="text-gray-100 text-3xl sm:text-4xl font-bold inline">Library App</h1>
             </div>
             <Link to="form">
-                <button className="bg-black bg-opacity-40 rounded">
+                <button className="bg-black bg-opacity-40 rounded sm:w-15 sm:h-15 self-center">
                     <img src={require("./images/plus.png")} alt="Add Book" className="w-13 sm:w-15" />
                 </button>
             </Link>
