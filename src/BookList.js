a5de099d560cb05caa4239d2f2c1e772f4e30593
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-09-11 18:38:37 -0400

    refreshes on window resize

--- src/BookList.js
+++ src/BookList.js
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 // props were passed down from parent component
 const BookList = ({books}) => {
+    const [WindowSize, setWindowSize] = useState(window.innerWidth);
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {setWindowSize(window.innerWidth)});
+        console.log('useEffect ran');
+    }, [WindowSize])
+
     const handleDelete = (id) => {
         fetch('http://localhost:8000/library/' + id, {
         method: 'DELETE'
@@ -14,11 +22,11 @@ const BookList = ({books}) => {
         let newString = '';
         let titleChars = Array.from(title);
 
-        if (window.innerWidth >= 640 && window.innerWidth < 768) {
+        if (WindowSize >= 640 && WindowSize < 768) {
             while (titleChars.length > 18) {
                 titleChars.pop();
             }
-        } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+        } else if (WindowSize >= 768 && WindowSize < 1024) {
             while (titleChars.length > 24) {
                 titleChars.pop();
             }
@@ -32,20 +40,20 @@ const BookList = ({books}) => {
         titleChars[titleChars.length - 2] = '.';
         titleChars[titleChars.length - 3] = '.';
         newString = titleChars.join('');
-        console.log(newString);
+        // console.log(newString);
         return newString;
     }
 
     return ( 
         <div>
             {books.map(book => (
-                    <div key={book.id} className={book.id % 2 === 0 ? "grid grid-cols-6 justify-center items-center py-4 bg-blue-50" : "grid grid-cols-6 justify-center items-center py-4"}
+                    <div key={book.id} className={book.id % 2 === 0 ? "grid grid-cols-6 justify-center items-center py-4 px-2 bg-blue-50" : "grid grid-cols-6 justify-center items-center py-4 px-2"}
                     id={`book-${book.id}`} onClick={() => {
                         // ideally I don't need to do this
                         let target = document.querySelector(`#book-${book.id}`).querySelector('.details');
                         let mainTitle = document.querySelector(`#book-${book.id}`).querySelector('.main-title');
                         target.classList.toggle('hidden');
-                        if (mainTitle.innerText !== ''){
+                        if (!target.classList.contains('hidden')){
                             mainTitle.innerText = '';
                         } else {
                             book.title.length > 14 ? mainTitle.innerText = shortenTitle(book.title) : mainTitle.innerText = book.title
@@ -65,10 +73,10 @@ const BookList = ({books}) => {
                             <img src={require("./images/delete.png")} alt="delete" className="w-10 h-10 sm:w-12 sm:h-12 my-1"/>
                         </button>
                         <div className="details hidden col-span-6">
-                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Title: ${book.title}` }</p>
-                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Author: ${book.author}` }</p>
-                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Pages: ${book.pages}` }</p>
-                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 ml-2">{ `Status: ${book.status}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 m-2 sm:m-3">{ `Title: ${book.title}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 m-2 sm:m-3">{ `Author: ${book.author}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 m-2 sm:m-3">{ `Pages: ${book.pages}` }</p>
+                            <p className="col-span-4 text-2xl sm:text-3xl font-medium px-1.5 m-2 sm:m-3">{ `Status: ${book.status}` }</p>
                         </div>
                     </div>
             ))}
