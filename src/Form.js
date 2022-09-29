f698ff86a5748c5df003e34c96c60b99e307957e
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-09-28 20:09:55 -0400

    adds clear form functionality

--- src/Form.js
+++ src/Form.js
@@ -64,34 +64,46 @@ const Form = ({ url, method }) => {
         })
     }
 
+    const handleClear = () => {
+        document.querySelector('form').reset();
+
+    }
+
     return ( 
-    <div className="sidebar lg:bg-gray-200 lg:flex lg:flex-col lg:h-full">
-        <Link to="/">
-            <button className="lg:hidden">
-                <img src={require("./images/window-close.png")} alt="close" className="w-11 sm: w-14 absolute top-4 right-4 z-20" />
-            </button>
-            {isEditing && <p className="text-right text-xl m-3 hover:text-pink-600 transition ease-out duration-300">Cancel</p>}
-            {!isEditing && <p className="text-right text-xl m-3 hover:text-pink-600 transition ease-out duration-300">Clear Form</p>}
-        </Link>
-        <form className="sidebar p-5 sm: p-8 lg:py-0 z-10 h-screen lg:h-full w-screen lg:w-full absolute lg:static top-0 flex flex-col justify-center" onSubmit={e => {
-            e.preventDefault();
-            handleSubmit();
-            }}>
-            <label htmlFor="title" className="formLabel">Title: </label>
-            <input type="text" className="formInput" defaultValue={defaultTitle} required onChange={e => setTitle(e.target.value)} id="title"/>
-            <label htmlFor="author" className="formLabel">Author: </label>
-            <input type="text" className="formInput" defaultValue={defaultAuthor} required onChange={e => setAuthor(e.target.value)} id="author" />
-            <label htmlFor="pages" className="formLabel">Pages: </label>
-            <input type="text" className="formInput" defaultValue={defaultPages} required onChange={e => setPages(e.target.value)} id="pages" />
-            <label htmlFor="status" className="formLabel">Read? </label>
-            <select name="status" className="formInput" defaultValue={defaultStatus} required onChange={e =>  setStatus(e.target.value)} id="status">
-                <option value="Read">I have read this book</option>
-                <option value="Unread">I have not read this book</option>
-            </select>
-            { !isPending && <button type="submit" className="p-2 bg-blue-600 hover:bg-blue-500 text-gray-100 rounded-sm sm:text-2xl mt-6 w-7/12 self-center transition ease-linear duration-300">Submit</button>}
-            { isPending && <button disabled>Processing</button>}
-        </form>
-    </div>
+        <div className="sidebar h-screen absolute w-screen top-0 bg-gray-200 lg:static lg:flex lg:flex-col lg:h-full">
+            <Link to="/">
+                <button className="lg:hidden">
+                    <img src={require("./images/window-close.png")} alt="close" className="w-11 sm: w-14 absolute top-6 left-6 z-20" />
+                </button>
+            </Link>
+            {isEditing && <div className="flex justify-between">
+                <Link to="/">
+                    <p className="text-right text-2xl lg:text-xl my-3 mr-4 lg:m-3 hover:text-pink-600 transition ease-out duration-300">Cancel</p>
+                </Link>
+                <p className="text-right text-2xl lg:text-xl my-3 mr-4 lg:m-3 hover:text-pink-600 transition ease-out duration-300" onClick={handleClear}>
+                Clear Form</p>
+            </div>}
+                {!isEditing && <p className="text-right text-2xl lg:text-xl my-3 mr-4 lg:m-3 hover:text-pink-600 transition ease-out duration-300" onClick={handleClear}>
+                Clear Form</p>}
+            <form className="sidebar p-5 sm: p-8 lg:py-0 z-10 h-screen lg:h-full w-screen lg:w-full lg:static top-0 flex flex-col justify-center" onSubmit={e => {
+                e.preventDefault();
+                handleSubmit();
+                }}>
+                <label htmlFor="title" className="formLabel">Title: </label>
+                <input type="text" className="formInput" placeholder={defaultTitle} required onChange={e => setTitle(e.target.value)} id="title"/>
+                <label htmlFor="author" className="formLabel">Author: </label>
+                <input type="text" className="formInput" placeholder={defaultAuthor} required onChange={e => setAuthor(e.target.value)} id="author" />
+                <label htmlFor="pages" className="formLabel">Pages: </label>
+                <input type="text" className="formInput" placeholder={defaultPages} required onChange={e => setPages(e.target.value)} id="pages" />
+                <label htmlFor="status" className="formLabel">Read? </label>
+                <select name="status" className="formInput" placeholder={defaultStatus} required onChange={e =>  setStatus(e.target.value)} id="status">
+                    <option value="Read">I have read this book</option>
+                    <option value="Unread">I have not read this book</option>
+                </select>
+                { !isPending && <button type="submit" className="p-2 bg-blue-600 hover:bg-blue-500 text-gray-100 rounded-sm sm:text-2xl mt-6 w-7/12 self-center transition ease-linear duration-300">Submit</button>}
+                { isPending && <button disabled>Processing</button>}
+            </form>
+        </div>
     );
 }
  
