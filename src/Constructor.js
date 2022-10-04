81d9324e674239fe34d70768c766d35dc28d69eb
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-10-03 22:54:37 -0400

    begins rework to switch to local storage

---
+++ src/Constructor.js
@@ -0,0 +1,11 @@
+class Book {
+    constructor (id, title, author, pages, status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+}
+
+export default Book;
